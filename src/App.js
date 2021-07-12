5770da9c4a9ccf4213c1af7aaddf918e021b506b
Matheus Cortez <matheus.cortez@live.com>
2021-07-12 00:06:34 -0300

    feat:fazendo get na api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react'
+import axios from 'axios'
+
+function App(props) {
+
+  const [usuario,setUsuario] = React.useState('')
+
+   function handlePesquisa(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`)
+    .then (response => console.log(response.data))
+    
+  
+    } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{props.title} {props.user}</h1>
+    <input className="" placeholder="Digite seu Usuario" value={usuario} onChange={(e)=> setUsuario(e.target.value)}/>
+    <button type="button" onClick={handlePesquisa}>Pesquisar</button>
+
+    <p>{usuario}</p>
+    </>
+  
   );
 }
 
