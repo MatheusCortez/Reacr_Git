d24a3ff5f27cd87fd666782a74bb9cace06c90a6
Matheus Cortez <matheus.cortez@live.com>
2021-07-12 11:23:38 -0300

    feat:routes config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 
 import React from 'react'
-import axios from 'axios'
+import Routes from './router';
 
-function App(props) {
+function App() {
 
-  const [usuario,setUsuario] = React.useState('')
 
-   function handlePesquisa(){
-    axios.get(`https://api.github.com/users/${usuario}/repos`)
-    .then (response => console.log(response.data))
-    
-  
-    } 
 
   return (
-    <>
-      <h1>{props.title} {props.user}</h1>
-    <input className="" placeholder="Digite seu Usuario" value={usuario} onChange={(e)=> setUsuario(e.target.value)}/>
-    <button type="button" onClick={handlePesquisa}>Pesquisar</button>
-
-    <p>{usuario}</p>
-    </>
+ 
+    <Routes/>
+   
   
   );
 }
---
+++ src/Pages/Repositories/Repositories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Repositories = () => {
+    return (
+        <div>
+            repositories
+        </div>
+    )
+}
+
+export default Repositories
